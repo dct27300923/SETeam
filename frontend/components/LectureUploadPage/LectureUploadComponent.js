6577e3a2f3cc08905f628f9227348ea7957fe852
dghg <dh@uos.ac.kr>
2021-12-10 18:42:56 +0900

    update ui

--- frontend/components/LectureUploadPage/LectureUploadComponent.js
+++ frontend/components/LectureUploadPage/LectureUploadComponent.js
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx }from '@emotion/react';
 import ContainerComponent from "../ContainerComponent";
 import styles from "../../styles/LectureUploadComponent.module.css";
 import {uploadLecture} from "../../utils/api-tools";
@@ -5,8 +7,8 @@ import {useState} from 'react';
 
 export default function LectureUploadComponent()
 {
-    let outline = <div className={styles.outline}>강의 업로드</div>
-    
+    let outline = <div>강의 업로드</div>
+
     let [uploadList, setUploadList] = useState([]);
     let title = <input type="text" placeholder="Title" 
         id="title" className={styles.title}></input>
@@ -14,9 +16,12 @@ export default function LectureUploadComponent()
     let buttons;
 
     const addUpload = () => {
-        uploadList.push(<div>
-            {title}
-            {file}
+        uploadList.push(<div className="card">
+            <div className="card-header text-info">제목과 파일을 입력해주세요.</div>
+            <div className="card-body">
+                {title}
+                {file}
+            </div>
         </div>); 
         uploadList = uploadList.slice();
         setUploadList(uploadList);
@@ -56,32 +61,49 @@ export default function LectureUploadComponent()
         {addBtn}
         {uploadBtn}
     </div>
-    let lectureUploadPage = <div>
-        {outline}
+    let lectureUploadPage = <div css={css`
+        & > * {
+            margin: 20px 0;
+        }
+    `}>
+        <div
+            className="card" 
+        >
+                <div className="card-header py-3">{outline}</div>
+                <div className="card-body"             
+                    css={css`
+                        & > * {
+                            margin-right: 10px;
+                        }
+                `}>
+                    <select name="week" id="week" className="btn btn-primary dropdown-toggle">
+                        <option value="0" disabled selected>주차 선택</option>
+                        <option value="1">1주차</option>
+                        <option value="2">2주차</option>
+                        <option value="3">3주차</option>
+                        <option value="4">4주차</option>
+                        <option value="5">5주차</option>
+                        <option value="6">6주차</option>
+                        <option value="7">7주차</option>
+                        <option value="8">8주차</option>
+                        <option value="9">9주차</option>
+                        <option value="10">10주차</option>
+                        <option value="11">11주차</option>
+                        <option value="12">12주차</option>
+                        <option value="13">13주차</option>
+                        <option value="14">14주차</option>
+                        <option value="15">15주차</option>
+                        <option value="16">16주차</option>
+                    </select>
+                    <select name="subject" id="subject" className="btn btn-primary dropdown-toggle">
+                        <option value="0" disabled selected>과목 선택</option>
+                        <option value="1">인공지능</option>
+                        <option value="2">소프트웨어응용</option>
+                        <option value="3">선형대수</option>
+                    </select>
+                </div>
+        </div>
         {uploadList}
-            <select name="week" id="week">
-                <option value="1">1주차</option>
-                <option value="2">2주차</option>
-                <option value="3">3주차</option>
-                <option value="4">4주차</option>
-                <option value="5">5주차</option>
-                <option value="6">6주차</option>
-                <option value="7">7주차</option>
-                <option value="8">8주차</option>
-                <option value="9">9주차</option>
-                <option value="10">10주차</option>
-                <option value="11">11주차</option>
-                <option value="12">12주차</option>
-                <option value="13">13주차</option>
-                <option value="14">14주차</option>
-                <option value="15">15주차</option>
-                <option value="16">16주차</option>
-            </select>
-            <select name="subject" id="subject">
-                <option value="1">인공지능</option>
-                <option value="2">소프트웨어응용</option>
-                <option value="3">선형대수</option>
-            </select>
         {buttons}
     </div>
     return (
